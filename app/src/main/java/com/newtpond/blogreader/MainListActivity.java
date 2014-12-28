51dff873bca7034a1336c357c0326b2be4766ea2
Martin Turjak <martinturjak@gmail.com>
2014-12-29 00:13:41 +0100

    ArrayAdapter to display a list of strings

--- app/src/main/java/com/newtpond/blogreader/MainListActivity.java
+++ app/src/main/java/com/newtpond/blogreader/MainListActivity.java
@@ -4,14 +4,33 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 
 public class MainListActivity extends ListActivity {
+
+    protected String[] mAndroidNames = {
+        "Alpha (1.0)",
+        "Beta (1.1)",
+        "Cupcake (1.5)",
+        "Donut (1.6)",
+        "Eclair (2.0–2.1)",
+        "Froyo (2.2–2.2.3)",
+        "Gingerbread (2.3–2.3.7)",
+        "Honeycomb (3.0–3.2.6)",
+        "Ice Cream Sandwich (4.0–4.0.4)",
+        "Jelly Bean (4.1–4.3.1)",
+        "KitKat (4.4–4.4.4)",
+        "Lollipop (5.0–5.0.2)"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mAndroidNames);
+        setListAdapter(adapter);
     }
 
 
